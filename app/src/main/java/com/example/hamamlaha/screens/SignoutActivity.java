11f5a41e9e934ea0c66bca13e1f2c6c8145d7bf6
shira <shiradahan130@gmail.com>
2025-12-24 09:28:17 +0200

    signout 24/12

--- app/src/main/java/com/example/hamamlaha/screens/SignoutActivity.java
+++ app/src/main/java/com/example/hamamlaha/screens/SignoutActivity.java
@@ -1,6 +1,10 @@
 package com.example.hamamlaha.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.hamamlaha.R;
+import com.example.hamamlaha.utils.SharedPreferencesUtil;
 
 public class SignoutActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignoutActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,20 @@ public class SignoutActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button buttonSignOut = findViewById(R.id.btn_signout_signout);
+        buttonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Sign out button clicked");
+                SharedPreferencesUtil.signOutUser(SignoutActivity.this);
+
+                Log.d(TAG, "User signed out, redirecting to LandingActivity");
+                Intent landingIntent = new Intent(SignoutActivity.this, LandingActivity.class);
+                landingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(landingIntent);
+            }
+        });
+
     }
 }
